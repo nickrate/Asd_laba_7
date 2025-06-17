52476b84185862bad8d443f226fa73410574a618
Artem <baldintop@gmail.com>
2025-06-17 16:04:39 +0300

    Добавьте файлы проекта.

---
+++ HashTable.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asd_laba_7
+{
+    internal class HashTable
+    {
+        private string[] table;
+        private int size;
+        private const string DELETED = "<deleted>";
+
+        public HashTable(int size)
+        {
+            this.size = size;
+            table = new string[size];
+        }
+
+        private int Hash(string key)
+        {
+            int hash = 0;
+            for (int i = 0; i < key.Length; i++)
+            {
+                hash += key[i] * (i + 1);
+            }
+            return hash % size;
+        }
+
+        public void Insert(string key)
+        {
+            int index = Hash(key);
+            int startIndex = index;
+            while (table[index] != null && table[index] != DELETED)
+            {
+                if (table[index] == key)
+                {
+                    Console.WriteLine("Такий ПІБ вже існує.");
+                    return;
+                }
+
+                index = (index + 1) % size;
+                if (index == startIndex)
+                {
+                    Console.WriteLine("Таблиця переповнена.");
+                    return;
+                }
+            }
+            table[index] = key;
+        }
+
+        public bool Search(string key)
+        {
+            int index = Hash(key);
+            int startIndex = index;
+            while (table[index] != null)
+            {
+                if (table[index] == key)
+                    return true;
+
+                index = (index + 1) % size;
+                if (index == startIndex)
+                    break;
+            }
+            return false;
+        }
+
+        public bool Delete(string key)
+        {
+            int index = Hash(key);
+            int startIndex = index;
+            while (table[index] != null)
+            {
+                if (table[index] == key)
+                {
+                    table[index] = DELETED;
+                    return true;
+                }
+                index = (index + 1) % size;
+                if (index == startIndex)
+                    break;
+            }
+            return false;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("\nХеш-таблиця:");
+            for (int i = 0; i < size; i++)
+            {
+                Console.WriteLine($"{i}: {(table[i] == null ? "<порожньо>" : table[i])}");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Text;
+
+namespace Asd_laba_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.InputEncoding = Encoding.UTF8;
+
+            Console.OutputEncoding = Encoding.UTF8;
+
+            HashTable table = new HashTable(11);
+
+            while (true)
+            {
+                Console.WriteLine("\nМеню:");
+                Console.WriteLine("1. Додати співробітника");
+                Console.WriteLine("2. Знайти співробітника");
+                Console.WriteLine("3. Видалити співробітника");
+                Console.WriteLine("4. Вивести хеш-таблицю");
+                Console.WriteLine("0. Вихід");
+                Console.Write("Ваш вибір: ");
+                var input = Console.ReadLine();
+
+                switch (input)
+                {
+                    case "1":
+                        Console.Write("Введіть ПІБ: ");
+                        string name = Console.ReadLine();
+                        table.Insert(name);
+                        break;
+                    case "2":
+                        Console.Write("Введіть ПІБ для пошуку: ");
+                        string searchName = Console.ReadLine();
+                        bool found = table.Search(searchName);
+                        Console.WriteLine(found ? "Знайдено!" : "Не знайдено.");
+                        break;
+                    case "3":
+                        Console.Write("Введіть ПІБ для видалення: ");
+                        string deleteName = Console.ReadLine();
+                        bool removed = table.Delete(deleteName);
+                        Console.WriteLine(removed ? "Видалено!" : "Не знайдено.");
+                        break;
+                    case "4":
+                        table.Print();
+                        break;
+                    case "0":
+                        return;
+                }
+            }
+        }
+    }
+}
